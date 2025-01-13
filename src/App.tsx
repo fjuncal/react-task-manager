3dbd8b4f490908301800e5422080a10bcf918615
Fellipe Juncal <fjuncal7@gmail.com>
2025-01-13 17:46:43 -0300

    Fazendo a chamada do componente de formulario de tarefas pro componente principal e estilizando o formulário

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,26 @@ import { useState } from "react";
 import GlobalStyles from "./styles/GlobalStyles";
 import { Task } from "./types/Task";
 import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([
     { id: 1, text: "Estudar React com TypeScript	", completed: false },
     { id: 2, text: "Fazer exercícios", completed: true },
   ]);
+
+  const addTask = (text: string) => {
+    const newTask = { id: tasks.length + 1, text, completed: false };
+
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <>
       <GlobalStyles />
       <div>
         <h1>Gerenciador de tarefas</h1>
+        <TaskForm onAddTask={addTask} />
         <TaskList task={tasks} />
       </div>
     </>
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { styled } from "styled-components";
 
 interface TaskFormProps {
   onAddTask: (text: string) => void;
@@ -18,16 +19,40 @@ export default function TaskForm({ onAddTask }: TaskFormProps) {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
+      <Form onSubmit={handleSubmit}>
+        <Input
           type="text"
           placeholder="Nova tarefa..."
           value={taskText}
           onChange={(e) => setTaskText(e.target.value)}
         />
 
-        <button type="submit">Adicionar</button>
-      </form>
+        <Button type="submit">Adicionar</Button>
+      </Form>
     </>
   );
 }
+
+const Form = styled.form`
+  display: flex;
+  gap: 8px;
+  margin-bottom: 16px;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  padding: 8px;
+  font-size: 16px;
+`;
+
+const Button = styled.button`
+  padding: 8px 16px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
